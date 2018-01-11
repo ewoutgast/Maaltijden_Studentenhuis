60376e68d377a458a925ab69d29d4fb438efbecf
Bart den Hoed <bartdhoed@gmail.com>
2018-01-11 20:24:40 +0100

    Add get meal functions

--- api/routes_v1.js
+++ api/routes_v1.js
@@ -1,12 +1,19 @@
+// Dependencies
 var express = require('express');
 var routes = express.Router();
+
+// Controllers
+var meal = require('../controller/meal.controller');
 
 module.exports = {}
 
 // routes
-app.use('*', function (req, res, next) {
+routes.use('*', function (req, res, next) {
     res.contentType('application/json'); // Add content type
     next();
 });
+
+routes.get('/meals', meal.getAll);
+routes.get('/meal/:id', meal.getById);
 
 module.exports = routes;
---
+++ controller/meal.controller.js
@@ -0,0 +1,40 @@
+var connection = require('../config/connection');
+
+module.exports = {
+    getAll(req, res, next) {
+        var query = 'SELECT * FROM meals';
+        
+        connection.query(query, function (error, rows, fields) {
+            if (error) {
+                next(error);
+            } else {
+                res.status(200).json({
+                    status: {
+                        query: 'OK'
+                    },
+                    result: rows
+                }).end();
+            };
+        });
+    },
+    
+    getById(req, res, next) {
+        const id = req.params.id;
+        
+        var query = 'SELECT * FROM meals WHERE id=' + id;
+        
+        connection.query(query, function (error, rows, fields) {
+            if (error) {
+                next(error);
+            } else {
+                res.status(200).json({
+                    status: {
+                        query: 'OK'
+                    },
+                    result: rows
+                }).end();
+            };
+        });
+        // res.end();
+    },
+};
