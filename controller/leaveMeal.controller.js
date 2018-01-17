c122e4ca6e4ea57d07802342002b3aa03b4cf9be
Bart den Hoed <bartdhoed@gmail.com>
2018-01-17 17:55:52 +0100

    Finish leave meal

--- controller/leaveMeal.controller.js
+++ controller/leaveMeal.controller.js
@@ -7,18 +7,7 @@ module.exports = {
          if (
              (typeof body.meal_id !== 'undefined' && body.meal_id) &&
              (typeof body.user_id !== 'undefined' && body.user_id)
-         ){
-             // Check user already joined
-            // console.log(checkUserIsJoined(body.meal_id, body.user_id));
-            if (!checkUserIsJoined(body.meal_id, body.user_id)) {
-                res.status(400).json({
-                    status: {
-                        message: 'User is not joined'
-                    }
-                }).end();
-                return false;
-            };
-             
+         ){ 
              // Leave meal by ID
              var query = 'DELETE FROM meals_users WHERE meal_id = ' + body.meal_id + ' AND user_id = ' + body.user_id;
 
@@ -36,22 +25,3 @@ module.exports = {
         };
     }
 };
-
-function checkUserIsJoined(meal_id, user_id) {
-    var query = 'SELECT id FROM meals_users WHERE meal_id = ' + meal_id + ' AND user_id = ' + user_id;
-    var result;
-
-    connection.query(query, function (error, rows, fields) {
-        if (error) {
-            console.log(error);
-            result = false;
-        } else if (rows.length != 1) {
-            result = false;
-        } else {
-            result = true;
-        };
-    });
-
-console.log(result);
-    return result;
-}
